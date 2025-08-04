31b39b3f8d0a462d134e69dd696f27d9e06ff89b
JosueCordero <97249861+JosueCordero@users.noreply.github.com>
2025-08-04 10:46:38 -0600

    feat: add a basic Buffer implementation

---
+++ include/minidb_pp/buffer.hpp
@@ -0,0 +1,21 @@
+#ifndef PLUS_MINI_DB_BUFFER_HPP
+#define PLUS_MINI_DB_BUFFER_HPP
+
+#include <cstddef>
+#include <string>
+
+class InputBuffer {
+    public:
+        InputBuffer() = default;
+        bool readInput();
+
+        const std::string& getBuffer() const;
+
+    private:
+        std::string buffer;
+        
+
+};
+
+#endif
+
---
+++ src/buffer.cpp
@@ -0,0 +1,16 @@
+#include "minidb_pp/buffer.hpp"
+#include <iostream>
+#include <string>
+
+
+
+bool InputBuffer::readInput(){
+    if(!std::getline(std::cin, this->buffer)){
+        return false;
+    } 
+    return true;   
+}
+
+const std::string& InputBuffer::getBuffer() const {
+    return buffer;
+}
